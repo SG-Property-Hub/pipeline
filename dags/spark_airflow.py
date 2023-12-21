bbd27c366a5d0e147223167ca84390d4c249ffdf
vietdoo <dodoviet@icloud.com>
2023-12-21 19:27:05 +0700

    fix all

--- dags/spark_airflow.py
+++ dags/spark_airflow.py
@@ -6,7 +6,7 @@ from airflow.providers.apache.spark.operators.spark_submit import SparkSubmitOpe
 dag = DAG(
     dag_id = "sparking_flow",
     default_args = {
-        "owner": "etsu",
+        "owner": "Yusuf Ganiyu",
         "start_date": airflow.utils.dates.days_ago(1)
     },
     schedule_interval = "@daily"
